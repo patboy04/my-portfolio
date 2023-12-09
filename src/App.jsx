37f023454b605f21594cb7570eb8d83bc3760e9d
patboy04 <theofficialjp@gmail.com>
2023-12-09 18:24:10 +0800

    added barebones home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar'
+import Home from './assets/components/Home'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
