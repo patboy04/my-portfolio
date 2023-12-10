cb832aa0d6baa123fec084b7934b4650c24054ae
patboy04 <theofficialjp@gmail.com>
2023-12-10 17:07:24 +0800

    added more styling to skills page and fixed bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import myImg from "../assets/me.jpg"
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-white flex flex-col justify-center lg:flex-row '>
+    <div name='about' className='w-full bg-white flex flex-col justify-center lg:flex-row '>
         <img src={myImg} className='px-20 mt-32 self-center md:px-60 lg:w-[40vw] lg:p-0 lg:m-20'/>
         <div className='lg:py-20 lg:pt-40 lg:text-3xl lg:my-32'>
             <h1 className='text-2xl text-center mt-20 pb-10 text-[#059669] md:text-5xl lg:mt-0'>John Patrick M. Lacsamana</h1>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,18 +5,46 @@ import js from "../assets/js.png"
 import react from "../assets/react.png"
 import java from "../assets/java.png"
 import mysql from "../assets/mysql.png"
+import firebase from "../assets/firebase.png"
+import flutter from "../assets/flutter.png"
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#D1FAE5] flex py-20 flex-col'>
-        <h1 className='text-center text-4xl sm:text-7xl py-8 text-[#059669]'>My Skills</h1>
-        <div className='grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 gap-14 md:gap-20 justify-items-center px-20 py-10'>
-            <img src={html} className='w-28'/>
-            <img src={css} className='w-28'/>
-            <img src={js} className='w-28'/>
-            <img src={react} className='w-28'/>
-            <img src={java} className='w-28'/>
-            <img src={mysql} className='w-28'/>
+    <div name='skills' className='w-full bg-[#D1FAE5] flex flex-col py-4 md:py-20'>
+        <h1 className='text-center text-4xl sm:text-7xl py-2 md:py-8 text-[#059669]'>My Skills</h1>
+        <div className='grid grid-cols-2 grid-rows-4 lg:grid-cols-4 lg:grid-rows-2 gap-6 lg:gap-y-10 justify-items-center px-16 lg:px-10 py-8 lg:py-5'>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500 overflow-hidden'>
+                <img src={html} className='w-28'/>
+                <h6>HTML</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={css} className='w-28'/>
+                <h6>CSS</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={js} className='w-28'/>
+                <h6>JAVASCRIPT</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={react} className='w-28'/>
+                <h6>REACT</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={firebase} className='w-28'/>
+                <h6>Firebase</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={java} className='w-28'/>
+                <h6>Java</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={flutter} className='w-28'/>
+                <h6>Flutter</h6>
+            </div>
+            <div className='shadow-md shadow-[#059669] hover:scale-110 duration-500'>
+                <img src={mysql} className='w-28'/>
+                <h6>MYSQL</h6>
+            </div>
         </div>
     </div>
   )
